4c679e9ca14e01cfd69f6a5836d82bd533346587
gahyun1113 <tlsrkgus1113@gmail.com>
2024-08-02 12:00:51 +0900

    dsf

---
+++ src/level1.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+
+public class level1 {
+    
+    static String[] question = {"1. Python에서 변수를 선언하는 방법은? (점수: 10점)","2. Python에서 리스트(List)의 특징은 무엇인가요? (점수: 15점)","3. Python에서 조건문을 작성하는 방법은? (점수: 10점)","4. Python에서 함수를 정의하는 방법은? (점수: 5점)"}; 
+    static String[] choice = {"1) var name 2) name = value 3) set name 4) name == value", "1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 \n" +
+            "상태로 유지된다", "1) if-else, 2) for-in, 3) while, 4) def", "1) class, 2) def, 3) import, 4) return"};
+    static int[] answer = {2,1,1,2};
+    static int[] score = {10,15,10,5};
+
+    static Scanner sc = new Scanner(System.in);
+    static Test[] tests = new Test[4];
+
+    
+    public static void main(String[] args) {
+       
+        int[] myAnswer = new int[4];
+        int myScore = 0;
+        char myGrade = 'F';
+
+        myScore = generateTest(myAnswer, myScore);
+        getResult(myAnswer, myScore, myGrade);
+
+    }
+
+    private static int generateTest(int[] myAnswer, int myScore) {
+        for (int i = 0; i < 4; i++) {
+            tests[i] = new Test(question[i], choice[i], answer[i], score[i]);
+            System.out.println(tests[i].getQuestion());
+            System.out.println(tests[i].getChoice());
+            System.out.print("- 정답 : ");
+            myAnswer[i] = sc.nextInt();
+
+            if (myAnswer[i] == tests[i].getAnswer()) {
+                myScore += tests[i].getScore();
+            }
+
+            System.out.println();
+        }
+
+        return myScore;
+    }
+
+    private static void getResult(int[] myAnswer, int myScore, char myGrade) {
+        System.out.println("-----------결과------------");
+        System.out.print("응답한 내용 : ");
+        for (int i = 0; i < 4; i++) {
+            System.out.print((i+1)+"번 "+ myAnswer[i] +", ");
+        }
+
+        System.out.println();
+        System.out.println("당신 응답 합계 : "+ myScore +"점");
+
+        if (myScore > 30) myGrade = 'A';
+        else if (myScore > 20) myGrade = 'B';
+        else if (myScore > 10) myGrade = 'C';
+
+        System.out.println("학점은 "+ myGrade +" 입니다.");
+    }
+}
